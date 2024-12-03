24ff17391b55a38838891610303a83eec62524ff
Izuchukwu Alaneme <izuchi.alaneme@gmail.com>
2024-12-03 14:46:53 +0100

    Footer, AboutUs, Toggle etc. refactored

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
-import { Route, Routes } from 'react-router-dom'
-import Footer from './components/footer/Footer'
-import NavigationBar from './components/Navbar/NavigationBar'
-import Home from './components/Home/Home'
-import Events from './components/Events/Events'
-import SchoolPortal from './components/SchoolPortal/SchoolPortal'
-import AboutUs from './components/AboutUs/AboutUs'
-import News from './components/News/News'
-import Pta from './components/P.T.A/Pta'
-import OurGalleryList from './components/OurGallery/OurGalleryList'
-import ToggleFormImage from './components/Home/ToggleFormIcon'
+import { Route, Routes } from 'react-router-dom';
+import Footer from './components/footer/Footer';
+import NavigationBar from './components/Navbar/NavigationBar';
+import Home from './components/Home/Home';
+import Events from './components/Events/Events';
+import SchoolPortal from './components/SchoolPortal/SchoolPortal';
+import AboutUs from './components/AboutUs/AboutUs';
+// import News from './components/News/News';
+// import Pta from './components/P.T.A/Pta';
+import OurGalleryList from './components/OurGallery/OurGalleryList';
+import ToggleFormImage from './components/Home/ToggleFormIcon';
+import Wildcard from './components/404Page/Wildcard';
+import './App.css';
 
 
 
@@ -17,19 +19,25 @@ function App() {
 
   return (
     <>
-    <NavigationBar />
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/school' element={<SchoolPortal />} />
-        <Route path='/aboutUs' element={<AboutUs />} />
-        <Route path='/events' element={<Events />} />
-        <Route path='/ourGallery' element={<OurGalleryList />} />
-        <Route path='/news' element={<News />} />
-        <Route path='/pta' element={<Pta />} />
+      <div className="app-wrapper"> {/* Set the footer to the bottom with */}
+        <NavigationBar />
+        <div className="content">
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/school' element={<SchoolPortal />} />
+            <Route path='/aboutUs' element={<AboutUs />} />
+            <Route path='/events' element={<Events />} />
+            <Route path='/ourGallery' element={<OurGalleryList />} />
+            {/* <Route path='/news' element={<News />} />
+            <Route path='/pta' element={<Pta />} /> */}
+            <Route path="*" element={<Wildcard/> } />
 
-      </Routes>
-      <ToggleFormImage/>
-    <Footer /> 
+          </Routes>
+          <ToggleFormImage/>
+          
+        </div>
+        <Footer />
+      </div>
     </>
   )
 }
